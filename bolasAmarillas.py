11267bac12740485bf516e8a532ddcc3931f6874
ricardo <nocive.science@gmail.com>
2024-03-17 16:24:45 -0300

    funciona deteniéndose las primeras tres bolas

--- bolasAmarillas.py
+++ bolasAmarillas.py
@@ -13,8 +13,11 @@ class Heat(Scene):
         ball=VGroup(*[
             self.get_ball(box,sign) for x,sign in zip(range(self.conf['n_part']),signatures)
         ])
-        self.add(box,ball)
-        self.wait(10)
+        title=self.get_title()
+        self.add(box,ball,title)
+        self.wait(4)
+        ball[:4].clear_updaters()
+        self.wait(2)
     def get_ball(self,box,sign):
         speed_factor=np.random.random()
         ball=Dot(radius=0.2,color=interpolate_color(BLUE,RED,speed_factor))
@@ -22,6 +25,7 @@ class Heat(Scene):
         sign.move_to(ball.get_center())
         sign.set_width(ball.get_width()-0.03)
         sign.set_color(BLACK)
+        ball.sign=sign
         ball.add(sign)
         speed=2+3*speed_factor
         direction=rotate_vector(RIGHT,TAU*np.random.random())
@@ -35,14 +39,31 @@ class Heat(Scene):
         ]))
         def update(ball,dt):
             ball.shift(ball.velocity*dt)
+            ball.sign.move_to(ball.get_center())
+            ball.sign.set_color(BLACK)
             if ball.get_left()[0]<box.get_left()[0]:
                 ball.velocity[0]=np.abs(ball.velocity[0])
+                ball.rotate(PI/4,about_point=ball.get_center())
+                ball.set_color(YELLOW)
             if ball.get_right()[0]>box.get_right()[0]:
                 ball.velocity[0]=-np.abs(ball.velocity[0])
+                ball.rotate(-PI/4,about_point=ball.get_center())
+                ball.set_color(RED)
             if ball.get_top()[1]>box.get_top()[1]:
                 ball.velocity[1]=-np.abs(ball.velocity[1])
+                ball.rotate(PI/4,about_point=ball.get_center())
+                ball.set_color(YELLOW)
             if ball.get_bottom()[1]<box.get_bottom()[1]:
                 ball.velocity[1]=np.abs(ball.velocity[1])
+                ball.rotate(PI/4,about_point=ball.get_center())
+                ball.set_color(RED)
             return ball
         ball.add_updater(update)
         return ball
+    def get_title(self):
+        title=Text('Bolas amarillas')        
+        title.set_stroke(width=0)
+        title.to_edge(UP)
+        new_title= CurvesAsSubmobjects(title[0])
+        new_title.set_color_by_gradient(YELLOW,RED)
+        return title
